4f16f57892d32e08ed00e1a97c32e342a1d1be75
codersparks <codersparks@users.noreply.github.com>
2024-12-18 19:26:15 +0000

    started to experiment with border_set

--- aoc-visualisation/src/grid.rs
+++ aoc-visualisation/src/grid.rs
@@ -31,6 +31,7 @@ impl Widget for GridCell {
 
         let border_set = symbols::border::Set {
             top_left: symbols::line::NORMAL.horizontal_down,
+            bottom_right: symbols::line::NORMAL.horizontal_up,
             ..symbols::border::PLAIN
         };
         let block = Block::default()
